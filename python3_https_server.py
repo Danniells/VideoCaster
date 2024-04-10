84cbc5af432782065b72cc4b55764b0bcb81b4e9
Danniells <danniells.bisaggio@gmail.com>
2024-04-10 19:52:01 -0300

    ...

--- python3_https_server.py
+++ python3_https_server.py
@@ -1,7 +1,7 @@
+#Para rodar o servidor, utilizar o comando sudo python3 -u "caminho/para/o/arquivo/do/servidor/python3_https_server.py"
 #Desative o servidor quando não estiver em uso:
 #Quando não estiver usando o servidor HTTP, desative-o para evitar que ele seja acessado por usuários não autorizados.
 #Use o comando kill para matar o processo do servidor HTTP.
-#Para rodar o servidor, utilizar o comando sudo python3 -u "caminho/para/o/arquivo/do/servidor/python3_https_server.py"
 
 import http.server
 import ssl
